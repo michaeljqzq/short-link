c0333c260d17808bb5d90dbc883d21a12253e58e
michaeljqzq <michaeljqzq@gmail.com>
2018-03-04 22:07:26 +0800

    Add script to deploy web

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,7 +8,7 @@ module.exports = [{
   entry: './src/index.js',
   output: {
     filename: 'mjstatic.bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist/web')
   },
   module: {
     rules: [
