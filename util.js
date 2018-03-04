7ecb6f048b67f772a54e78782cdcbd56b15a8c40
michaeljqzq <michaeljqzq@gmail.com>
2018-03-04 19:29:05 +0800

    Update logic to read config

--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
+import path from 'path';
 
-let configs = fs.readFileSync('./config.json');
+let configs = fs.readFileSync(path.resolve(__dirname, 'config.json'));
 
 export default JSON.parse(configs);
