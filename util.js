6b7b8176bc29e0c15f4e8676c4ecfa9295c182c0
huaimi <huaimi@pin.com>
2019-11-05 20:21:02 +0800

    Change config file url

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-let configs = fs.readFileSync(path.resolve(__dirname, 'config.json'));
+let configs = fs.readFileSync('/etc/short-link/config.json');
 
 export default JSON.parse(configs);
